43c5eb5bb6285a779fa8438b62dcfc8d143f0c2a
rhindottire <achmadaliridho46@gmail.com>
2025-01-31 03:50:34 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black flex justify-center items-center min-h-screen">
+        <h1 className="text-3xl font-bold underline text-white">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
